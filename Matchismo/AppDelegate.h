ddfa30c8ee2095b71fd77b3d327b5706047ad856
陈志鹏 <chenzhipeng@edaijia-inc.cn>
2015-08-24 22:54:32 +0800

    init

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by 陈志鹏 on 8/24/15.
+//  Copyright (c) 2015 陈志鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by 陈志鹏 on 8/24/15.
+//  Copyright (c) 2015 陈志鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
